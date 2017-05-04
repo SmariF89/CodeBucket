b3d6c33fc0b8fd90f72eb40aa7bd24804775f5d5
bjarki15 <bjarki15@ru.is>
2017-05-04 14:34:27 +0000

    _user farid

--- Codebucket/Services/ProjectService.cs
+++ Codebucket/Services/ProjectService.cs
@@ -22,8 +22,8 @@ namespace Codebucket.Services
         public List<ProjectViewModel> getAllProjectsByApplicationUserId(string userName)
         {
             //var projectIds = (from i in _db._projectMembers
-            //              where (i._userName == userName)
-            //              select i._projectID);
+            //                  where (i._userName == userName)
+            //                  select i._projectID);
 
             //List<Project> projects = new List<Project>();
             //foreach (int i in projectIds)
