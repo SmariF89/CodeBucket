2b0aa3056ee3610532aaf079b08a8ce4d3c86746
Davíð Gunnarsson <eliseix@gmail.com>
2017-05-05 20:24:14 +0000

    for pull

--- Codebucket/Models/ViewModels/ProjectFileViewModel.cs
+++ Codebucket/Models/ViewModels/ProjectFileViewModel.cs
@@ -17,6 +17,6 @@ namespace Codebucket.Models.ViewModels
         public string _projectFileType { get; set; }
         public string _projectFileData { get; set; }
 
-        public int ProjectID { get; set; }
+        public int _projectID { get; set; }
     }
 }
