8d4e0504d7253bbe3974788b1dc7128234ec67db
SnorriArinbjarnar <snorriarinbjarnar@gmail.com>
2017-05-07 15:48:17 +0000

    Create new file validation

--- Codebucket/Controllers/ProjectFileController.cs
+++ Codebucket/Controllers/ProjectFileController.cs
@@ -21,32 +21,35 @@ namespace Codebucket.Controllers
 		[HttpGet]
 		public ActionResult createNewProjectFile(int? id)
 		{
-            CreateProjectFileViewModel model = new CreateProjectFileViewModel();
-            model._projectID = id.Value;
+			CreateProjectFileViewModel model = new CreateProjectFileViewModel();
+			model._projectID = id.Value;
 			return View(model);
 		}
 	
-        // POST: createNewProjectFile
-        [HttpPost]
-        public ActionResult createNewProjectFile(CreateProjectFileViewModel model)
-        {
-            model._projectFileType = _projectFileService.getFileTypeByProjectId(model._projectID);
-            model._projectFileData = "";
+		// POST: createNewProjectFile
+		[HttpPost]
+		public ActionResult createNewProjectFile(CreateProjectFileViewModel model)
+		{
+			model._projectFileType = _projectFileService.getFileTypeByProjectId(model._projectID);
+			model._projectFileData = "";
 
-            if (ModelState.IsValid)
-            {
+			if (!ModelState.IsValid)
+			{
+                CreateProjectFileViewModel viewModel = new CreateProjectFileViewModel();
+                viewModel._projectFileName = model._projectFileName;
+
+                return View("createNewProjectFile", viewModel);
+            }
+			else
+			{
                 _projectFileService.addProjectFile(model);
                 ProjectViewModel viewModel = _projectService.getProjectByProjectId(model._projectID);
                 //return RedirectToAction("displayProject", "ProjectFile", new { model._projectID });
                 return View("displayProject", viewModel);
             }
-            else
-            {
-                return View("createNewProjectFile", model);
-            }
 
-            
-        }
+			
+		}
 		#endregion
 
 		#region Update file in current project.
@@ -76,60 +79,60 @@ namespace Codebucket.Controllers
 		#endregion
 
 		[HttpGet]
-        public ActionResult displayProject(int? id)
-        {
-            currentProjectId = id;
-            ProjectViewModel model = _projectService.getProjectByProjectId(currentProjectId);
+		public ActionResult displayProject(int? id)
+		{
+			currentProjectId = id;
+			ProjectViewModel model = _projectService.getProjectByProjectId(currentProjectId);
 
-            return View(model);
-        }
+			return View(model);
+		}
 
-        #region List all files in current project.
-        // GET: getProjectFileById
-        [HttpGet]
-        public ActionResult listAllProjectFiles(int? id)
-        {
-            currentProjectId = id;
-            return View(_projectFileService.getAllProjectFilesByProjectId(currentProjectId));
-        }
-        #endregion
+		#region List all files in current project.
+		// GET: getProjectFileById
+		[HttpGet]
+		public ActionResult listAllProjectFiles(int? id)
+		{
+			currentProjectId = id;
+			return View(_projectFileService.getAllProjectFilesByProjectId(currentProjectId));
+		}
+		#endregion
 
-        #region Add member to current project.   
-        // GET: AddProjectMember
-        [HttpGet]
-        public ActionResult addProjectMember(int? id)
-        {
-            AddMemberViewModel model = new AddMemberViewModel();
-            model._projectID = id.Value;
+		#region Add member to current project.   
+		// GET: AddProjectMember
+		[HttpGet]
+		public ActionResult addProjectMember(int? id)
+		{
+			AddMemberViewModel model = new AddMemberViewModel();
+			model._projectID = id.Value;
 
-            return View(model);
-        }
+			return View(model);
+		}
 
-        // POST: AddProjectMember
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult addProjectMember(AddMemberViewModel model)
-        {
-            int currId = model._projectID;
+		// POST: AddProjectMember
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult addProjectMember(AddMemberViewModel model)
+		{
+			int currId = model._projectID;
 
-            if (!ModelState.IsValid)
-            {
-                AddMemberViewModel viewModel = new AddMemberViewModel();
-                viewModel._userName = model._userName;
+			if (!ModelState.IsValid)
+			{
+				AddMemberViewModel viewModel = new AddMemberViewModel();
+				viewModel._userName = model._userName;
 
-                return View("addProjectMember", viewModel);
-            }
+				return View("addProjectMember", viewModel);
+			}
 
-            else
-            {
+			else
+			{
 
-                _projectService.addProjectMember(model);
-                ProjectViewModel model2 = _projectService.getProjectByProjectId(currId);
+				_projectService.addProjectMember(model);
+				ProjectViewModel model2 = _projectService.getProjectByProjectId(currId);
 
-                return View("displayProject", model2);
-            }
-            
-        }
+				return View("displayProject", model2);
+			}
+			
+		}
 
 		#endregion
 
