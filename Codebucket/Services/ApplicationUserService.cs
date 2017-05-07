7ce6928f10e9779cb9f199d9fd49c091883c263e
Davíð Gunnarsson <eliseix@gmail.com>
2017-05-07 17:00:55 +0000

    fixing code

--- Codebucket/Services/ApplicationUserService.cs
+++ Codebucket/Services/ApplicationUserService.cs
@@ -12,38 +12,45 @@ namespace Codebucket.Services
     {
         private ApplicationDbContext _db;
 
+        // Constructor, makes a new instance of database connection.
         public ApplicationUserService()
         {
             _db = new ApplicationDbContext();
         }
 
-        public ApplicationUserViewModel getApplicationUserById(int? id)
+        // Get all project owners by project ID, Returns a list of the entitiy ProjectOwner.
+        public List<ProjectOwner> getAllProjectOwnersByProjectId(int? id)
+        {            
+            if (id != null || id > 0)
+            {
+                return (from projectOwner in _db._projectOwners
+                        where projectOwner._projectID == id
+                        select projectOwner).ToList();
+            }
+            return null;            
+        }
+
+        // Get all project members by project ID, returns a list of the entity ProjectMember.
+        public List<ProjectMember> getAllProjectMembersByProjectId(int? id)
         {
+            if (id != null || id > 0)
+            {
+                return (from projectMember in _db._projectMembers
+                    where projectMember._projectID == id
+                    select projectMember).ToList();
+            }
             return null;
         }
 
-        public List<ProjectOwner> getAllProjectOwnersByProjectId(int? id)
-        {
-            List<ProjectOwner> newProjectOwner = (from projectOwner in _db._projectOwners
-                                                     where projectOwner._projectID == id
-                                                     select projectOwner).ToList();
-            return newProjectOwner;
-        }
-
-        public List<ProjectMember> getAllProjectMembersByProjectId(int? id)
-        {
-            List<ProjectMember> newProjectMember = (from projectMember in _db._projectMembers
-                                                    where projectMember._projectID == id
-                                                    select projectMember).ToList();
-            return newProjectMember;
-        }
-
+        // Get all project members by project id, returns a list of viewModel ProjectMemberViewModel.
         public List<ProjectMemberViewModel> getAllProjectMemberViewModelsByProjectId(int? id)
         {
             List<ProjectMemberViewModel> newProjectMemberViewModel = new List<ProjectMemberViewModel>();
-            List<ProjectMember> newProjectMember = new List<ProjectMember>();
+            List<ProjectMember> newProjectMember = getAllProjectMembersByProjectId(id);
 
-            newProjectMember = getAllProjectMembersByProjectId(id);
+            //List<ProjectMember> newProjectMember = new List<ProjectMember>();
+            //newProjectMember = getAllProjectMembersByProjectId(id);
+
 
             foreach (var item in newProjectMember)
             {
