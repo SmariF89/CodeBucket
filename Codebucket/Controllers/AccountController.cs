73191065cb71cca2929f5ed1d7859add5ff1f26c
leifur16 <leifur16@ru.is>
2017-05-08 20:52:19 +0000

    small fix

--- Codebucket/Controllers/AccountController.cs
+++ Codebucket/Controllers/AccountController.cs
@@ -87,7 +87,7 @@ namespace Codebucket.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt2.");
+                    ModelState.AddModelError("", "Invalid username or password.");
                     return View(model);
             }
         }
