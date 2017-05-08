90fd763d3825df4bfd7fde93b9f6c2ac100e0ab7
clover89 <clover1810@gmail.com>
2017-05-08 14:55:35 +0000

    Projects overview overhaul

--- Codebucket/Scripts/Test.js
+++ Codebucket/Scripts/Test.js
@@ -1,1 +1,14 @@
-﻿
+﻿$(document).ready(function () {
+    $(".movie-views .btn input:radio").change(function () {
+        if ($(this).val() == 'poster') {
+            $('.movies-list').fadeOut('fast', function () {
+                $('.movies-poster').fadeIn('fast');
+            });
+        }
+        else {
+            $('.movies-poster').fadeOut('fast', function () {
+                $('.movies-list').fadeIn('fast');
+            });
+        }
+    });
+});
